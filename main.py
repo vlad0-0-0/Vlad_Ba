757c820e83038961b40eb257842a7fddf7edf4bc
Vlad <vladc2088@gmail.com>
2025-04-13 13:02:56 +0300

    Lesson 1 - Создание первого echo bot

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('My first project')
+from aiogram import Bot, Dispatcher, types
+import asyncio
+from aiogram.filters import CommandStart
+
+TOKEN = '7587755001:AAGZ9bfc_kxW7q9-LVSkO4RVQS4rgxivnIA'
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: types.Message):
+    await message.answer('Привет это бот по баскетбольным мячам.')
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer('Бот пока находится в разработке.')
+    user_text = message.text
+    await message.answer(user_text)
+
+async def main():
+    print('Бот запущен.')
+    await dp.start_polling(bot)
+
+
+
+asyncio.run(main())
